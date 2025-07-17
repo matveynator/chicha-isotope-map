d05799e19e58e2cd7f245c4b91defebb9cb9aff6
matvey <matvey@copters.ru>
2025-07-17 09:41:26 +0300

    stable release: fix 386

--- pkg/database/driver-genji.go
+++ pkg/database/driver-genji.go
@@ -1,4 +1,4 @@
-//go:build dragonfly ios || freebsd || darwin || (linux && ppc64) || (linux && ppc64le) || (linux && s390x) || (linux && amd64) || (linux && mips64) || (linux && mips64le) || (linux && arm64) || android || (windows && amd64) || (windows && arm64)
+//go:build dragonfly || ios || freebsd || darwin || (linux && ppc64) || (linux && ppc64le) || (linux && s390x) || (linux && amd64) || (linux && mips64) || (linux && mips64le) || (linux && arm64) || android || (windows && amd64) || (windows && arm64)
 
 package database
 
